ec27224706e3eca935fd8461127b0379c277f7e9
baozier <byn9826@gmail.com>
2016-10-30 17:53:15 -0400

    Add files via upload

--- example/Rating.jsx
+++ example/Rating.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Rate from 'thousanday-react';
+import {Rate} from 'thousanday-react';
 
 class Rating extends Component{
 	constructor(){
