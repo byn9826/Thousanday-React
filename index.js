01f73de3d9fb2aa56657cfc65d7635d28c07ef57
baozier <byn9826@gmail.com>
2016-11-10 17:17:15 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -12,8 +12,15 @@ var _Inputbox = require('./lib/Inputbox/Inputbox.js');
 
 var _Inputbox2 = _interopRequireDefault(_Inputbox);
 
+var _Inputarea = require('./lib/Inputbox/Inputarea.js');
+
+var _Inputarea2 = _interopRequireDefault(_Inputarea);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = {
-	Rate: _Rate2.default, Upvote: _Upvote2.default, Inputbox: _Inputbox2.default
+	Rate: _Rate2.default,
+	Upvote: _Upvote2.default,
+	Inputbox: _Inputbox2.default,
+	Inputarea: _Inputarea2.default
 };
