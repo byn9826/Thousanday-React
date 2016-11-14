79ba794467814e110ddd78eb0448c999cb28639e
baozier <byn9826@gmail.com>
2016-11-14 13:18:56 -0500

    Add files via upload

---
+++ src/inputbox/Inputarea.jsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+class Inputarea extends Component{
+  constructor(props){
+		super(props);
+		this.state = {
+      width:this.props.width||"100%",
+      border:this.props.border||"1px solid orange",
+      height:this.props.height||"auto",
+      fontSize:this.props.fontSize||"13px",
+      content:this.props.content||"",
+      count:parseInt(this.props.total)-this.props.content.length,
+      length:parseInt(this.props.total),
+      name:this.props.name||"",
+      restrict:this.props.restrict||"on"
+		};
+	}
+  editInput(event){
+    if (this.state.restrict=="off"){
+      this.setState({content:event.target.value});
+      this.setState({count:this.state.length-event.target.value.length});
+    }
+    else {
+      let changedInput = event.target.value.substr(0,this.state.length);
+      this.setState({content:changedInput});
+      this.setState({count:this.state.length-changedInput.length});
+    }
+  }
+	render(){
+    let spanStyle={
+      width:this.state.width,
+      display:"inline-block"
+    }
+    let inputStyle={
+      float:"left",
+      marginBottom:"10px",
+      width:"100%",
+      border:this.state.border,
+      height:this.state.height,
+      fontFamily:"Arial",
+      fontSize:this.state.fontSize
+    };
+    let countStyle={
+      float:"left",
+      fontFamily:"Arial",
+      fontSize:"11px",
+      width:"100%"
+    }
+		return(
+			<span style={spanStyle}>
+				<textarea name={this.state.name} style={inputStyle} value={this.state.content} onChange={this.editInput.bind(this)} />
+        <span style={countStyle}>{this.state.count}/{this.state.length}</span>
+			</span>
+		)
+	}
+};
+export default Inputarea;
---
+++ src/rate/Rate.jsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+class Rate extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			rate:parseInt(this.props.rate),
+			change:this.props.change||"no",
+			length:this.props.length||5,
+			className:this.props.className
+		};
+	}
+	mouseOver(event){
+		this.setState({rate:parseInt(event.target.id)});
+	}
+	render(){
+		let spanStyle;
+		if(this.state.change=="yes"){
+			spanStyle={
+				color: this.props.color||"orange",
+				fontSize:this.props.font||"18px",
+				cursor :"pointer",
+				fontFamily:"Arial"
+			};
+		}
+		else{
+			spanStyle={
+				color: this.props.color||"orange",
+				fontSize:this.props.font||"18px",
+				fontFamily:"Arial"
+			};
+		}
+		let stars=[];
+		for(var i=1;i<=this.props.rate;i++){
+			if(this.state.change=="yes"){
+				stars.push(<span key={"black"+i} id={i} onMouseOver={this.mouseOver.bind(this)} onClick={this.props.rateChange.bind(null,this.state.rate)} >★</span>);
+			}
+			else{
+				stars.push(<span key={"black"+i} id={i}>★</span>);
+			}
+		}
+		for(var j=this.props.rate*1+1;j<=this.state.length;j++){
+			if(this.state.change=="yes"){
+				stars.push(<span key={"white"+j} id={j} onMouseOver={this.mouseOver.bind(this)} onClick={this.props.rateChange.bind(null,this.state.rate)} >☆</span>);
+			}
+			else{
+				stars.push(<span key={"white"+j} id={j}>☆</span>);
+			}
+		}
+		return(
+			<span className={this.props.className} style={spanStyle}>
+				{stars}
+			</span>
+		)
+	}
+};
+export default Rate;
