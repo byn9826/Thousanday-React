ef73e0104ddc652ef93fd358e83482f0f1046ea6
baozier <byn9826@gmail.com>
2016-11-14 13:19:32 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 'use strict';
 
-var _Rate = require('./lib/Rate/Rate.js');
+var _Rate = require('./lib/rate/Rate.js');
 
 var _Rate2 = _interopRequireDefault(_Rate);
 
-var _Upvote = require('./lib/Upvote/Upvote.js');
+var _Upvote = require('./lib/upvote/Upvote.js');
 
 var _Upvote2 = _interopRequireDefault(_Upvote);
 
-var _Inputbox = require('./lib/Inputbox/Inputbox.js');
+var _Inputbox = require('./lib/inputbox/Inputbox.js');
 
 var _Inputbox2 = _interopRequireDefault(_Inputbox);
 
-var _Inputarea = require('./lib/Inputbox/Inputarea.js');
+var _Inputarea = require('./lib/inputbox/Inputarea.js');
 
 var _Inputarea2 = _interopRequireDefault(_Inputarea);
+
+var _Imagebox = require('./lib/imagebox/Imagebox.js');
+
+var _Imagebox2 = _interopRequireDefault(_Imagebox);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -22,5 +26,6 @@ module.exports = {
 	Rate: _Rate2.default,
 	Upvote: _Upvote2.default,
 	Inputbox: _Inputbox2.default,
-	Inputarea: _Inputarea2.default
+	Inputarea: _Inputarea2.default,
+	Imagebox: _Imagebox2.default
 };
