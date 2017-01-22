d6417be71261ec6d9546f1ac19eef0a34d3897af
baozier <byn9826@gmail.com>
2017-01-22 01:32:36 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,49 +8,29 @@ var _Rate = require('./lib/attitude/Rate.js');
 
 var _Rate2 = _interopRequireDefault(_Rate);
 
-var _Upvote = require('./lib/upvote/Upvote.js');
-
-var _Upvote2 = _interopRequireDefault(_Upvote);
-
-var _Inputbox = require('./lib/inputbox/Inputbox.js');
-
-var _Inputbox2 = _interopRequireDefault(_Inputbox);
-
-var _Inputarea = require('./lib/inputbox/Inputarea.js');
+var _Inputarea = require('./lib/input/Inputarea.js');
 
 var _Inputarea2 = _interopRequireDefault(_Inputarea);
 
-var _Imagebox = require('./lib/imagebox/Imagebox.js');
+var _Inputbox = require('./lib/input/Inputbox.js');
 
-var _Imagebox2 = _interopRequireDefault(_Imagebox);
+var _Inputbox2 = _interopRequireDefault(_Inputbox);
 
-var _Droplist = require('./lib/droplist/Droplist.js');
+var _Vote = require('./lib/attitude/Vote.js');
 
-var _Droplist2 = _interopRequireDefault(_Droplist);
+var _Vote2 = _interopRequireDefault(_Vote);
 
-var _Upsertlist = require('./lib/upsertlist/Upsertlist.js');
+var _AddtoList = require('./lib/list/AddtoList.js');
 
-var _Upsertlist2 = _interopRequireDefault(_Upsertlist);
-
-var _Progress = require('./lib/progress/Progress.js');
-
-var _Progress2 = _interopRequireDefault(_Progress);
-
-var _Editit = require('./lib/editit/Editit.js');
-
-var _Editit2 = _interopRequireDefault(_Editit);
+var _AddtoList2 = _interopRequireDefault(_AddtoList);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = {
 	Random: _Random2.default,
 	Rate: _Rate2.default,
-	Upvote: _Upvote2.default,
-	Inputbox: _Inputbox2.default,
 	Inputarea: _Inputarea2.default,
-	Imagebox: _Imagebox2.default,
-	Droplist: _Droplist2.default,
-	Upsertlist: _Upsertlist2.default,
-	Progress: _Progress2.default,
-	Editit: _Editit2.default
+	Inputbox: _Inputbox2.default,
+	Vote: _Vote2.default,
+	AddtoList: _AddtoList2.default
 };
